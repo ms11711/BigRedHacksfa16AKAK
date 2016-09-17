03d05364c5ccc18966dc7e33683e4f1e1a744534
Greg Schultz <gjs86@cornell.edu>
2016-09-17 17:07:59 -0400

    front page stuff

---
+++ php/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title> Home </title>
+		<link href="../css/stylesheet1.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<?php include "../snippets/header.php";?>
+	</body>
+</html>
---
+++ snippets/header.php
@@ -0,0 +1,5 @@
+<div id="header">
+	<img src="../img/hands2.png" alt="hands" id="homeLogo">
+	<input class="entry" id="password" type="text" name="password" placeholder="Password">
+	<input class="entry" id="username" type="text" name="username" placeholder="Username">
+</div> <!-- close header-->
